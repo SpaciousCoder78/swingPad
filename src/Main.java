eef2ca69aa6e435c97a742aac8d36b120a9541bb
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2022-12-21 11:17:03 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,9 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.PrintWriter;
 
-
 public class Main {
     public static void main(String[] args) {
         app obj = new app();
-
     }
 }
 
@@ -25,8 +23,38 @@ class app extends JFrame  {
 
         ActionListener al = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String body= t1.getText();
-                String filepath="textfile.txt";
+                String body = t1.getText();
+                filesaver fs = new filesaver(body);
+            }
+        };
+
+        b.addActionListener(al);
+
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class filesaver {
+    String body;
+    JFrame frame;
+
+    public filesaver(String body) {
+        this.body = body;
+        frame = new JFrame();
+
+        JLabel l2= new JLabel("Enter the file name: ");
+        JTextField t3 = new JTextField(20);
+        JButton b= new JButton("Save file");
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String filepath=t3.getText();
                 try {
                     FileWriter fw = new FileWriter(filepath, true);
                     BufferedWriter bw = new BufferedWriter(fw);
@@ -35,23 +63,16 @@ class app extends JFrame  {
                     pw.println(body);
                     pw.flush();
                     pw.close();
+                    JOptionPane.showMessageDialog(null,"Record Saved");
+
                 }
                 catch (Exception x){
-
                 }
             }
-        };
-
-
-        b.addActionListener(al);
-
-
-
-        setLayout(new FlowLayout());
-        setVisible(true);
-        setSize(400,400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        });
+        frame.add(l2);
+        frame.add(t3);
+        frame.add(b);
     }
-
-
 }
+
