8f12564ba1a46507fa90ba57afa99bd8fa69f8a8
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2022-12-24 21:29:32 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main {
+    //Calling app class
     public static void main(String[] args) {
         app obj = new app();
     }
@@ -16,6 +19,7 @@ public class Main {
 class app   {
 
     public app(){
+        //Creating new frame
         JFrame x = new JFrame("swingPad");
         JTextArea textArea = new JTextArea(45,150);
 
@@ -23,6 +27,7 @@ class app   {
         x.add(textArea, BorderLayout.CENTER);
 
         ActionListener ax = new ActionListener() {
+            //Calling app method again for new file option
             public void actionPerformed(ActionEvent e) {
                 app newobj = new app();
             }
@@ -30,34 +35,90 @@ class app   {
 
         ActionListener al = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                //Saving text as txt file
                 String body = textArea.getText();
                 filesaver fs = new filesaver(body);
+            }
+        };
+
+        ActionListener ab= new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app newoby = new app();
+                fileopener obj2 = new fileopener(textArea);
+            }
+        };
+
+        ActionListener bc = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame z = new JFrame("Info");
+                JLabel l2= new JLabel("swingPad v1.3 ");
+                JLabel l3= new JLabel("Created by SpaciousCoder78 @ GitHub ");
+                z.add(l2);
+                z.add(l3);
+                z.setLayout(new FlowLayout());
+                z.setVisible(true);
+                z.setSize(400,400);
+
+
             }
         };
 
 
 
         JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenu info = new JMenu("Info");
+        JMenu fileMenu = new JMenu("File"); //Menu bar item: File
+        JMenu info = new JMenu("Info");// Menu bar item: Info
         menuBar.add(fileMenu);
+        menuBar.add(info);
 
-        JMenuItem saveMenuItem = new JMenuItem("Save File");
-        JMenuItem newMenuItem = new JMenuItem("New File");
-        fileMenu.add(newMenuItem);
+        JMenuItem saveMenuItem = new JMenuItem("Save File");// File menu option: Save file
+        JMenuItem newMenuItem = new JMenuItem("New File");// File menu option:New file
+        JMenuItem OpenFileItem = new JMenuItem("Open File");// File menu option: Open File
+        fileMenu.add(newMenuItem);//adding all the options to file menu
         fileMenu.add(saveMenuItem);
+        fileMenu.add(OpenFileItem);
+        JMenuItem infobox = new JMenuItem("Info");
+        info.add(infobox);
         x.setJMenuBar(menuBar);
-        saveMenuItem.addActionListener(al);
-        newMenuItem.addActionListener(ax);
+        saveMenuItem.addActionListener(al);//calling saving menu
+        newMenuItem.addActionListener(ax);//calling new file
+        OpenFileItem.addActionListener(ab);
+        infobox.addActionListener(bc);
+
         x.setLayout(new FlowLayout());
 
 // Create a text area
 
 
         x.setVisible(true);
-        x.setSize(1280,720);
+        x.setSize(400,400);
         x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
+}
+
+class fileopener {
+    public fileopener(JTextArea textArea) {
+        // Open a file chooser dialog and allow the user to select a text file to open
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Open Text File");
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            // Read the contents of the selected file and display them in the text area
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileChooser.getSelectedFile()))) {
+                textArea.setText(""); // clear the text area
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    textArea.append(line + "\n");
+                }
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
 }
 
 class filesaver {
